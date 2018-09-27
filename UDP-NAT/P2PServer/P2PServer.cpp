62cc828e8d2548ea7974db0aa650dfe834c75807
CoderAldrich <CoderAldrich@gmail.com>
2018-09-27 15:39:26 +0800

    修复编译错误

--- UDP-NAT/P2PServer/P2PServer.cpp
+++ UDP-NAT/P2PServer/P2PServer.cpp
@@ -231,8 +231,7 @@ int main(int argc, char* argv[])
 								break;
 							}
 						}
-						if(removeiterator != NULL)
-							ClientList.remove(*removeiterator);
+						ClientList.remove(*removeiterator);
 						break;
 					}
 				case P2PTRANS:
